aa72330274462057525431f25fceb61d74f0b245
ArmandoFalcucci <110033547+ArmandoFalcucci@users.noreply.github.com>
2025-01-12 13:12:31 +0100

    nat perf

--- script/outline-analysis.R
+++ script/outline-analysis.R
@@ -26,6 +26,7 @@ lf2 <- lf[!basename(lf) %in% paste0(files_to_remove, ".txt")]
 # Output the remaining files after removal
 print(lf2)
 
+library(readxl)
 
 # 2. Modifying the dataset ----
 dataset <- read_excel("data/dataset.xlsx")
@@ -36,10 +37,10 @@ dataset <- read_excel("data/dataset.xlsx")
 #   filter(!ID %in% ids_to_exclude)
 
 # 3. Extracting coordinates and attaching IDs ----
-Coordinates <- import_txt(lf2)
-Coordinates_perforation <- dataset %>%
-  pull("ID") %>%
-  as.character()
+Coordinates <- import_txt(lf)
+# Coordinates_perforation <- dataset %>%
+#   pull("ID") %>%
+#   as.character()
 
 ids_to_remove <- c("EXP_63", "EXP_82", "EXP_87", "EXP_133", "EXP_138", "EXP_172", "EXP_183")
 
@@ -48,9 +49,9 @@ dataset.filtered <- dataset %>%
   filter(!(ID %in% ids_to_remove))
 
 # Pull the IDs from the dataset and convert them to character
-Coordinates_perforation <- dataset %>%
-  pull("ID") %>%
-  as.character()
+# Coordinates_perforation <- dataset %>%
+#   pull("ID") %>%
+#   as.character()
 
 Coordinates_perforation <- dataset.filtered %>%
   pull("ID") %>%
@@ -58,7 +59,7 @@ Coordinates_perforation <- dataset.filtered %>%
 
 
 Coordinates_2DGM <- Coordinates[Coordinates_perforation]
-GM_perforation <- Out(Coordinates_2DGM, fac = select(dataset.filtered, c("Type")))
+GM_perforation <- Out(Coordinates_2DGM, fac = select(dataset.filtered, c("Type", "Perforation")))
 
 # 4. GMM procedures to centre, scale, and rotate the coordinates ----
 GM_perforation_centered <- Momocs::coo_centre(GM_perforation)
@@ -66,11 +67,18 @@ GM_perforation_centered_scaled <- Momocs::coo_scale(GM_perforation_centered)
 GM_perforation_centered_scaled <- Momocs::coo_slidedirection(GM_perforation_centered_scaled)
 # GM_perforation_centered_scaled_rotated <- Momocs::coo_rotatecenter(GM_perforation_centered_scaled, theta = -pi/2)
 
+
+# stack inspections
+
+stack(GM_perforation_centered_scaled)
+
 # 5. Calibrating harmonic power ----
 harmonic_power <- calibrate_harmonicpower_efourier(GM_perforation_centered_scaled)
 
+harmonic_power
+
 # 6. EFA analysis ----
-GM_perforation_centered_scaled.EFA <- efourier(GM_perforation_centered_scaled, nb.h = 53, smooth.it = 0, norm = T)
+GM_perforation_centered_scaled.EFA <- efourier(GM_perforation_centered_scaled, nb.h = 43, smooth.it = 0, norm = T)
 
 # 7. PCA (Principal Component Analysis) ----
 GM_perforation_centered_scaled.PCA <- PCA(GM_perforation_centered_scaled.EFA)
@@ -102,7 +110,7 @@ ggsave(GM_bladelets_shape_variation_flipped,
        width = 5, height = 5, dpi = 300, units = "in", device = "png")
 
 #10 Merging datasets to produce figures and run other analyses ####
-GM_PCScores <- as.data.frame(GM_perforation_centered_scaled_rotated.PCA$x)
+GM_PCScores <- as.data.frame(GM_perforation_centered_scaled.PCA$x)
 
 GM_PCScores$ID <- dataset.filtered$ID
 
@@ -174,6 +182,12 @@ Means_PC1_PC3.Type <- dataset.filtered %>%                                      
                    PC2 = mean(PC2),
                    PC3 = mean(PC3))
 
+Means_PC1_PC3.Perforation <- dataset.filtered %>%                                               #subset dataset by Artifact Class and calculate means for PCs for each Class
+  group_by(Perforation) %>% 
+  dplyr::summarise(PC1 = mean(PC1),
+                   PC2 = mean(PC2),
+                   PC3 = mean(PC3))
+
 PC1toPC2.Type <- ggplot(data = dataset.filtered, aes(x = PC1, y = PC2, color = Type)) +
   geom_point(size = 2.5, alpha = 0.5) +
   labs(y= "PC2 (14% of total variance)", x = "PC1 (62% of total variance)") +
@@ -201,6 +215,75 @@ PC1toPC2.Type <- ggplot(data = dataset.filtered, aes(x = PC1, y = PC2, color = T
   # scale_color_manual(labels=c("A1 (n = 171)", "A2 (n = 198)"), values=A1_A2_palette)
 
 PC1toPC2.Type
+
+
+PC1toPC2.Perf <- ggplot(data = dataset.filtered, aes(x = PC1, y = PC2, color = Perforation)) +
+  geom_point(size = 2.5, alpha = 0.5) +
+  labs(y= "PC2 (14% of total variance)", x = "PC1 (62% of total variance)") +
+  geom_hline(yintercept = 0, 
+             linetype = "dashed", 
+             color = "black",
+             size = 1, 
+             alpha = 0.5) +
+  geom_vline(xintercept = 0, 
+             linetype = "dashed", 
+             color = "black",
+             size = 1, 
+             alpha = 0.5)  +
+  geom_point(data = Means_PC1_PC3.Perforation,
+             aes(x = PC1, y = PC2),
+             size = 4.5, shape = 16) +
+  guides(colour = guide_legend(override.aes = list(size=5))) +
+  theme_pubclean() +
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size=14),
+        axis.title.x = element_text(size = 14, hjust = -0.2),
+        axis.title.y = element_text(size = 14),
+        axis.text = element_text(size=12))
+
+PC1toPC2.Perf
+
+
+PC1_Perf <- ggplot(data = dataset.filtered, aes(x = Perforation, y = PC1, color = Perforation)) +
+  geom_boxplot(alpha = 0.5, outlier.shape = NA) +  # Create the boxplot and hide default outliers
+  geom_jitter(size = 2.5, alpha = 0.5, width = 0.1, height = 0.1) +  # Add jittered points
+  labs(y = "PC1 (62% of total variance)", x = "Perforation") +
+  geom_hline(yintercept = 0, 
+             linetype = "dashed", 
+             color = "black",
+             size = 1, 
+             alpha = 0.5) +
+  theme_pubclean() +
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size=14),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text = element_text(size=12))
+
+PC1_Perf
+
+
+PC1_Type <- ggplot(data = dataset.filtered, aes(x = Type, y = PC1, color = Type)) +
+  geom_boxplot(alpha = 0.5, outlier.shape = NA) +  # Create the boxplot and hide default outliers
+  geom_jitter(size = 2.5, alpha = 0.5, width = 0.1, height = 0.1) +  # Add jittered points
+  labs(y = "PC1 (62% of total variance)", x = "Perforation") +
+  geom_hline(yintercept = 0, 
+             linetype = "dashed", 
+             color = "black",
+             size = 1, 
+             alpha = 0.5) +
+  theme_pubclean() +
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size=14),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text = element_text(size=12))
+
+PC1_Type
+
 
 
 library(ggplot2)
@@ -276,6 +359,11 @@ print(PC1toPC2.Type.Hull)
 library(ggsci)
 
 plot.momocs <-  plot_PCA(GM_perforation_centered_scaled.PCA, axes = c(1, 2), ~ Type, morphospace = TRUE, points = TRUE, zoom = 0.9, chull = FALSE,  palette = pal_jama(), legend = TRUE) %>% layer_chullfilled(alpha = 0.8) %>% layer_legend(cex = 2/6) # Plot the PCA
+
+
+plot.momocs <-  plot_PCA(GM_perforation_centered_scaled.PCA, axes = c(1, 2), ~ Perforation, morphospace = TRUE, points = TRUE, zoom = 0.9, chull = FALSE,  palette = pal_jama(), legend = TRUE) %>% layer_chullfilled(alpha = 0.8) %>% layer_legend(cex = 2/6) # Plot the PCA
+
+plot.momocs <-  plot_PCA(GM_perforation_centered_scaled.PCA, axes = c(1, 2), ~ Perforation, morphospace = TRUE, points = TRUE, zoom = 0.9, chull = FALSE, legend = T) %>% layer_chullfilled(alpha = 0.8) %>% layer_legend(cex = 2/6) # Plot the PCA
 
 
 # Open a PDF device to save the plot
@@ -429,31 +517,35 @@ dev.off()
 
 
 # 13. PERMANOVA ----
-min_n_PCs.2DGM <- Momocs::scree_min(GM_bladelets_centered_scaled_rotated.PCA, prop = 0.95) 
+min_n_PCs.2DGM <- Momocs::scree_min(GM_perforation_centered_scaled.PCA, prop = 0.95) 
 
 # Create the Y matrix of variables under comparison:
-Y.PERMANOVA <- Dataset_2DGM[, c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9")]
+Y.PERMANOVA <- dataset.filtered[, c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9")]
 
 # Perform one-way PERMANOVA
-GM_PERMANOVA.layer <- vegan::adonis2(Y.PERMANOVA ~ Dataset_2DGM$Layer, method = "euclidean", permutations = 10000)
-GM_PERMANOVA.rawmatsource <- vegan::adonis2(Y.PERMANOVA ~ Dataset_2DGM$raw.material.lumped, method = "euclidean", permutations = 10000)
-GM_PERMANOVA.rawmatprov <- vegan::adonis2(Y.PERMANOVA ~ Dataset_2DGM$raw.material.provenience, method = "euclidean", permutations = 10000)
+GM_PERMANOVA.type <- vegan::adonis2(Y.PERMANOVA ~ dataset.filtered$Type, method = "euclidean", permutations = 10000)
+
+GM_PERMANOVA.perforation <- vegan::adonis2(Y.PERMANOVA ~ dataset.filtered$Perforation, method = "euclidean", permutations = 10000)
+
+# GM_PERMANOVA.rawmatprov <- vegan::adonis2(Y.PERMANOVA ~ Dataset_2DGM$raw.material.provenience, method = "euclidean", permutations = 10000)
 
 # Pairwise differences:
-Pairwise_PERMANOVA.rawmatsource <- pairwiseAdonis::pairwise.adonis(Y.PERMANOVA, Dataset_2DGM$raw.material.lumped, sim.method = "euclidean", p.adjust.m = "bonferroni", perm = 10000)
+Pairwise_PERMANOVA.type <- pairwiseAdonis::pairwise.adonis(Y.PERMANOVA, dataset.filtered$Type, sim.method = "euclidean", p.adjust.m = "bonferroni", perm = 10000)
+
+Pairwise_PERMANOVA.perf <- pairwiseAdonis::pairwise.adonis(Y.PERMANOVA, dataset.filtered$Perforation, sim.method = "euclidean", p.adjust.m = "bonferroni", perm = 10000)
 
 # 14. Disparity Test ----
-Dataset_2DGM$Layer_RawMat <- paste(Dataset_2DGM$Layer, Dataset_2DGM$raw.material.lumped, sep = ", ")
-Dataset_2DGM$Layer_RawMat <- dplyr::recode(Dataset_2DGM$Layer_RawMat, "A1, Local" = "A1, Local", "A2, Local" = "A2, Local", "A1, Circum.local" = "A1, Non-local", "A2, Circum.local" = "A2, Non-local", "A1, Distant" = "A1, Non-local", "A2, Distant" = "A2, Non-local", "A1, Very.distant" = "A1, Non-local", "A2, Very.distant" = "A2, Non-local") %>%
-  fct_relevel("A1, Local", "A2, Local", "A1, Non-local", "A2, Non-local")
+# dataset.filtered$Layer_RawMat <- paste(Dataset_2DGM$Layer, Dataset_2DGM$raw.material.lumped, sep = ", ")
+# data$Layer_RawMat <- dplyr::recode(Dataset_2DGM$Layer_RawMat, "A1, Local" = "A1, Local", "A2, Local" = "A2, Local", "A1, Circum.local" = "A1, Non-local", "A2, Circum.local" = "A2, Non-local", "A1, Distant" = "A1, Non-local", "A2, Distant" = "A2, Non-local", "A1, Very.distant" = "A1, Non-local", "A2, Very.distant" = "A2, Non-local") %>%
+#   fct_relevel("A1, Local", "A2, Local", "A1, Non-local", "A2, Non-local")
 
 # Ensure Dataset_2DGM is a data frame or matrix
-Dataset_2DGM <- as.data.frame(Dataset_2DGM)
+dataset.filtered <- as.data.frame(dataset.filtered)
 
 # Subset the columns correctly (make sure PC1 to PC9 are the correct column names)
-PC.data.subset.GM <- Dataset_2DGM[, c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9")]
+PC.data.subset.GM <- dataset.filtered[, c("PC1", "PC2", "PC3", "PC4", "PC5")]
 
-rownames_DATASETS <- as.factor(Dataset_2DGM$Layer_RawMat)
+rownames_DATASETS <- as.factor(dataset.filtered$Type)
 
 # Disparity test 
 data_subsets <- dispRity::custom.subsets(PC.data.subset.GM, group = rownames_DATASETS)
